6362ef306b81ed48f21ca21422f116bcaf569525
Dzmitry Belash <Dzmitry.Belash@gmail.com>
2020-11-17 16:05:30 +0300

    Replace findViewById with view binding in home fragment

--- app/src/main/java/com/example/alf/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/alf/ui/home/HomeFragment.kt
@@ -4,28 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.example.alf.R
+import androidx.fragment.app.viewModels
+import com.example.alf.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var binding: FragmentHomeBinding
+
+    private val homeViewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
     }
 }
