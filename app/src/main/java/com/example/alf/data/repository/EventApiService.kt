f05002ba23973959a76af0f5eb148bf72fca0f0c
Dzmitry Belash <Dzmitry.Belash@gmail.com>
2020-12-01 23:43:08 +0200

    Implement event API service

---
+++ app/src/main/java/com/example/alf/data/repository/EventApiService.kt
@@ -0,0 +1,75 @@
+package com.example.alf.data.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.alf.data.model.event.Event
+import com.example.alf.network.ApiClient
+import com.example.alf.network.EventApiInterface
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class EventApiService {
+
+    private var eventApiInterface: EventApiInterface = ApiClient.getApiClient().create(EventApiInterface::class.java)
+
+    init {
+        eventApiInterface = ApiClient.getApiClient().create(EventApiInterface::class.java)
+    }
+
+    fun fetchAllEvents(): LiveData<List<Event>> {
+        val data = MutableLiveData<List<Event>>()
+
+        eventApiInterface.fetchAllEvents().enqueue(object : Callback<List<Event>> {
+
+            override fun onFailure(call: Call<List<Event>>, t: Throwable) {
+                data.value = null
+            }
+
+            override fun onResponse(
+                    call: Call<List<Event>>,
+                    response: Response<List<Event>>
+            ) {
+                val res = response.body()
+                if (response.code() == 200 && res != null) {
+                    data.value = res
+                } else {
+                    data.value = null
+                }
+            }
+        })
+
+        return data
+    }
+
+    fun updateEvent(resultLiveData: MutableLiveData<Boolean?>, event: Event): LiveData<Boolean?>{
+
+        eventApiInterface.updateEvent(event.id, event).enqueue(object : Callback<Event>{
+            override fun onFailure(call: Call<Event>, t: Throwable) {
+                resultLiveData.value = false
+            }
+
+            override fun onResponse(call: Call<Event>, response: Response<Event>) {
+                resultLiveData.value = (response.code() == 200 || response.code() == 204)
+            }
+        })
+
+        return resultLiveData
+    }
+
+    fun deleteEvent(resultLiveData: MutableLiveData<Boolean?>, event: Event): LiveData<Boolean?> {
+
+        eventApiInterface.deleteEvent(event.id).enqueue(object : Callback<Unit> {
+            override fun onFailure(call: Call<Unit>, t: Throwable) {
+                resultLiveData.value = false
+            }
+
+            override fun onResponse(call: Call<Unit>, response: Response<Unit>) {
+                resultLiveData.value = (response.code() == 200 || response.code() == 204)
+            }
+        })
+
+        return resultLiveData
+    }
+
+}
--- app/src/main/java/com/example/alf/network/EventApiInterface.kt
+++ app/src/main/java/com/example/alf/network/EventApiInterface.kt
@@ -2,11 +2,16 @@ package com.example.alf.network
 
 import com.example.alf.data.model.event.Event
 import retrofit2.Call
-import retrofit2.http.GET
+import retrofit2.http.*
 
 interface EventApiInterface {
 
     @GET("events")
     fun fetchAllEvents(): Call<List<Event>>
 
+    @PUT("events/{id}")
+    fun updateEvent(@Path("id") id: Int, @Body event: Event): Call<Event>
+
+    @DELETE("events/{id}")
+    fun deleteEvent(@Path("id") id: Int): Call<Unit>
 }
