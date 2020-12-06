8cebfd87d7a06cde85d562de2300b7b04826f843
Dzmitry Belash <Dzmitry.Belash@gmail.com>
2020-12-06 23:03:29 +0200

    Make field position optional

--- app/src/main/java/com/example/alf/data/model/match/MatchPerson.kt
+++ app/src/main/java/com/example/alf/data/model/match/MatchPerson.kt
@@ -3,5 +3,5 @@ package com.example.alf.data.model.match
 data class MatchPerson (
     var player: Player,
     var role: Role,
-    var fieldPosition: FieldPosition
+    var fieldPosition: FieldPosition?
 )
