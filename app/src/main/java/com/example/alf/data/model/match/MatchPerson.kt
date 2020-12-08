2a834b22c9cec5052c4fd221810a61c8854de7b3
Dzmitry Belash <Dzmitry.Belash@gmail.com>
2020-12-08 17:55:28 +0200

    Add substitutions to match persons

--- app/src/main/java/com/example/alf/data/model/match/MatchPerson.kt
+++ app/src/main/java/com/example/alf/data/model/match/MatchPerson.kt
@@ -2,6 +2,8 @@ package com.example.alf.data.model.match
 
 data class MatchPerson (
     var player: Player,
-    var role: Role,
-    var fieldPosition: FieldPosition?
+    var playerRole: Role,
+    var fieldPosition: FieldPosition?,
+    var inForPlayer: Player?,
+    var outForPlayer: Player?
 )
