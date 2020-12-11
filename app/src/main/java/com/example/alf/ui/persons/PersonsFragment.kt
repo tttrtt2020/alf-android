7d05f2e01b4871ff0b5f437aac9d4a85b86287a4
Dzmitry Belash <Dzmitry.Belash@gmail.com>
2020-12-11 20:26:19 +0200

    Refactoring

--- app/src/main/java/com/example/alf/ui/persons/PersonsFragment.kt
+++ app/src/main/java/com/example/alf/ui/persons/PersonsFragment.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.os.Bundle
 import android.view.*
 import android.widget.SearchView
-import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
