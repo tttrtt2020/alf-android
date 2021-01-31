5eafbc40ee11942b3355be22ff1002dd669f1168
Dzmitry Belash <Dzmitry.Belash@gmail.com>
2021-01-31 22:47:03 +0200

    Small fix

--- app/src/main/java/com/example/alf/MainActivity.kt
+++ app/src/main/java/com/example/alf/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.appBarMain.toolbar)
 
